3872e671eaf60093caabee8aeee657a70324c095
bogdanlutsenko <1danya0443@gmail.com>
2017-08-29 19:13:48 -0400

    test

--- src/test/java/com/fourstay/runners/CukesRunner.java
+++ src/test/java/com/fourstay/runners/CukesRunner.java
@@ -11,7 +11,7 @@ import cucumber.api.junit.Cucumber;
 		features = "src/test/resources/features", 
 		glue = "com/fourstay/step_definitions", 
 		tags = "@Staytest_125",
-		dryRun = false
+		dryRun = true
 		
 		/* clean version
 		 * changed dryRun to true
